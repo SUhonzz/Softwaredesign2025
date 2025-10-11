d498d58881d562907b399973c7b942e21234ad9b
SUhonzz <uh5864@mci4me.at>
2025-10-11 15:46:44 +0200

    feat: Removed first two .py files which were consolidate into Jupyter Notebook

--- B-02.py
+++
@@ -1,5 +0,0 @@
-band_members = ["Peter", "Bjorn", "John"]
-
-for member in band_members:
-    print(f"{member} played great in this song.")
-print(f"I can not wait to hear {member} play in the next song.") #after loop finished member contains last item
